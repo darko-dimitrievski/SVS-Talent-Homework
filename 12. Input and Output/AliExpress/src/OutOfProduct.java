2bcf755fbc7e72b9086b667f6069667a8a374777
Darko <dare_rm@hotmail.com>
2015-03-24 10:24:34 +0100

    Revert "SVS-Talent-Homework"
    This reverts commit 0aca7a58ccbeaa329a2172445c2acee59389b788.

--- 12. Input and Output/AliExpress/src/OutOfProduct.java
+++
@@ -1,7 +0,0 @@
-
-public class OutOfProduct extends Exception {
-	public OutOfProduct() {
-		// TODO Auto-generated constructor stub
-		super("We don't have so much articles of that product in the warehouse. We are sorry");
-	}
-}
